9290c80d49be755e4f7ef7ac4201ba4cd6e312ef
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-08-08 23:35:39 -0400

    added dotenv

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 import React,{useState,useEffect} from 'react';
-import './App.css';
 import Papa from 'papaparse'
-// import csvdb from 'csv-database'
+
+import MapboxGLMap from './MapboxGLMap'
 
 const App=() => {
-  // const csvHeaders=[
-  //   'unique_mos_id',	
-  //   'first_name',	
-  //   'last_name',	
-  //   'command_now',	
-  //   'complaint_id',	
-  //   'month_received',	
-  //   'year_received',	
-  //   'month_closed',	
-  //   'year_closed',	
-  //   'command_at_incident',	
-  //   'rank_abbrev_incident',	
-  //   'rank_abbrev_now',	
-  //   'rank_now',	
-  //   'rank_incident',	
-  //   'mos_ethnicity',	
-  //   'mos_gender',	
-  //   'mos_age_incident',	
-  //   'complainant_ethnicity',	
-  //   'complainant_gender',	
-  //   'complainant_age_incident',	
-  //   'fado_type',	
-  //   'allegation',	
-  //   'precinct',	
-  //   'contact_reason',	
-  //   'outcome_description',
-  //   'board_disposition',
-  // ]
 
 const [data,setData] = useState(null)
 useEffect(()=>{
@@ -46,7 +18,7 @@ useEffect(()=>{
   return (
     <div className="App">
       <h1>ayoo</h1>
-      {console.log(data)}
+      <MapboxGLMap />
     </div>
   );
 }
---
+++ src/MapboxGLMap.js
@@ -0,0 +1,45 @@
+import React,{useState,useEffect,useRef} from 'react'
+import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+require('dotenv').config()
+
+const styles = {
+  width: "100vw",
+  height: "calc(100vh - 80px)",
+  position: "absolute"
+};
+
+const MapboxGLMap = ()=>{
+    const mapContainer = useRef(null)
+    const [map, setMap] = useState(null);
+    const [mapState,setMapState]=useState({
+        lng: 0,
+        lat: 0,
+        zoom: 2
+    })
+    useEffect(()=>{
+        mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
+        const initializeMap=({setMap,mapContainer})=>{
+            const map = (new mapboxgl.Map(
+                {
+                    container: mapContainer.current,
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    center: [mapState.lng, mapState.lat],
+                    zoom: mapState.zoom
+                }
+            ))
+            map.on("load", () => {
+                setMap(map);
+                map.resize();
+            });
+        }
+          if (!map) initializeMap({ setMap, mapContainer });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[map])
+
+    return(
+        <div ref={el=>mapContainer.current=el} style={styles} />
+    )
+}
+
+export default MapboxGLMap
